e81119e74b76aba5ee56cddcbc2fd940e76ada4c
pawel-jasiaczyk <pawel.jasiaczyk@gmail.com>
2017-05-03 11:37:41 +0200

    First Commit

---
+++ Parameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przelewy24
+{
+    public class Parameter
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public Parameter(string name, string value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+    }
+}
---
+++ Przelewy24.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Przelewy24
+{
+    public class Przelewy24
+    {
+
+        public static string CalculateSign (string[] inputFields)
+        {
+            StringBuilder stb = new StringBuilder ();
+
+            for (int i = 0; i < inputFields.Length; i++)
+            {
+                stb.Append (inputFields[i]);
+                if (i < inputFields.Length - 1)
+                    stb.Append ("|");
+            }
+
+            return CalculateMD5Hash (stb.ToString ());
+        }
+
+        public static string CalculateMD5Hash (string input)
+        {
+            MD5 md5 = MD5.Create ();
+            byte[] inputBytes = Encoding.ASCII.GetBytes (input);
+
+            byte[] hash = md5.ComputeHash (inputBytes);
+
+            StringBuilder stb = new StringBuilder ();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                stb.Append (hash[i].ToString ("X2"));
+            }
+
+            return stb.ToString ();
+        }
+    }
+}
