ff3e14b367c873170d3b51d42240a07b99e5b585
Pawe³ Jasiaczyk <pawel.jasiaczyk@gmail.com>
2017-05-04 20:50:36 +0200

    Add option of ignore null values in CalculateSign, default value is 'false'.

--- Przelewy24.cs
+++ Przelewy24.cs
@@ -10,18 +10,28 @@ namespace Przelewy24
     public class Przelewy24
     {
 
-        public static string CalculateSign (string[] inputFields)
+        public static string CalculateSign (string[] inputFields, bool ignonreNulls)
         {
             StringBuilder stb = new StringBuilder ();
 
             for (int i = 0; i < inputFields.Length; i++)
             {
+                if (inputFields[i] == null)
+                    if (ignonreNulls)
+                        continue;
+                    else
+                        throw new NullReferenceException("Parameter inputFields position: " + i);
                 stb.Append (inputFields[i]);
                 if (i < inputFields.Length - 1)
                     stb.Append ("|");
             }
 
             return CalculateMD5Hash (stb.ToString ());
+        }
+
+        public static string CalculateSign(string[] inputFields)
+        {
+            return CalculateSign(inputFields, false);
         }
 
         public static string CalculateMD5Hash (string input)
